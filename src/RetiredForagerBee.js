c5fd549ce099393bd48de5ed718a1b2e016ce242
Jae Shin <jae5464@gmail.com>
2016-06-16 19:15:53 -0700

    Implement subclassing with ES6 syntax

--- src/RetiredForagerBee.js
+++ src/RetiredForagerBee.js
@@ -1,7 +1,19 @@
 import ForagerBee from './ForagerBee';
 
-class RetiredForagerBee {
-  // TODO..
+class RetiredForagerBee extends ForagerBee {
+  constructor() {
+    super()
+    this.age = 40
+    this.color = 'grey'
+    this.job = 'gamble'
+    this.canFly = false
+  }
+  forage() {
+    return 'I am too old, let me play cards instead';
+  }
+  gamble(treasure) {
+    super.forage(treasure)
+  }
 };
 
 export default RetiredForagerBee;
